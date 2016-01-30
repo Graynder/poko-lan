6e01c1dc772c5cbdf7e1e61f0a21b2d430eab9a7
Graynder <florianbappel@gmail.com>
2016-01-30 17:39:16 +0100

    Dynamisation / communication entre les boutons front-end et le serveur

--- app/controllers/socket.js
+++ app/controllers/socket.js
@@ -5,6 +5,19 @@ var express = require('express'),
 module.exports  = function (io, app) {
 
     io.on('connection', function (socket) {
-      socket.emit('news', app.locals.game);
+        socket.emit('news', app.locals.game);
+
+        socket.on('fold',function () {
+            console.log("fold");
+        });
+        socket.on('check',function () {
+            console.log("check");
+        });
+        socket.on('call',function () {
+            console.log("call");
+        });
+        socket.on('rise',function () {
+            console.log("rise");
+        });
     });
 }
--- public/js/game.js
+++ public/js/game.js
@@ -1,19 +1,52 @@
 (function($){
 
- var ipServer = $("#ipServ").text();
- console.log(ipServer);
+    var ipServer = $("#ipServ").text();
+    var dataGame;
 
-var socket = initSocket(ipServer)
- socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
+    var socket = initSocket(ipServer)
+
+    socket.on('news', function (data) {
+        console.log(data);
+        dataGame = data;
+    });
+
+    var slide = $( "#slider-range-min" ).slider({
+        range: "min",
+        value: 0,
+        min: 1,
+        max: 700,
+        slide: function( event, ui ) {
+            $( "#Rise" ).text( "Rise " + ui.value + " €");
+        }
+    });
+
+    $("#Fold").click(function() {
+        console.log("fold");
+        socket.emit("fold");
+
+    });
+
+    $("#Check").click(function() {
+        console.log("check");
+        socket.emit("check");
+
+    });
+
+    $("#Call").click(function() {
+        console.log("call");
+        socket.emit("call");
+
+    });
+
+    $("#Rise").click(function() {
+        console.log("rise");
+        socket.emit("rise");
+
+    });
 
 })(jQuery);
 
 function initSocket(addr) {
-    //console.log('http://' + addr + ':3000/');
     //Connection au socket
     return io.connect('http://' + addr + ':3000/');
-    //return io.connect('http://localhost:3000');
 }
